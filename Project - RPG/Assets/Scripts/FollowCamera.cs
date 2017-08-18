7963d0f9dcf3e9e881bcc72d0d35becd3617d220
jahooligan50 <jefseaff@gmail.com>
2017-08-18 09:48:53 -0700

    camera now folows player movement

---
+++ Project - RPG/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        playerTransform = GameObject.FindObjectOfType<PlayerMovement>().GetComponentInChildren<Rigidbody2D>().GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerTransform)
+        {
+            transform.position = playerTransform.position + new Vector3 (0,0,-10f);
+        }
+	}
+}

7f0359dc6ede64e27e6c2beb9ab9a96720ff9ea0
jahooligan50 <jefseaff@gmail.com>
2017-08-18 01:00:27 -0700

    added player movement animation

---
+++ Project - RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody2D rBody;
+    private Vector2 moveVector;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        rBody = GetComponentInChildren<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); //raw axis is on or off, not gradual
+
+        if (moveVector != Vector2.zero)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("input_x", moveVector.x);
+            anim.SetFloat("input_y", moveVector.y);
+        }
+        else anim.SetBool("isWalking", false);
+
+        rBody.MovePosition(rBody.position + moveVector * Time.deltaTime * moveSpeed);
+	}
+
+
+
+}

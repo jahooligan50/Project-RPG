a587116a4ae3d4452a5562bd409b1fa6334de782
jahooligan50 <jefseaff@gmail.com>
2017-08-29 20:52:51 -0700

    dracky in, animations up, enemy movement/combat not up yet

---
+++ Project - RPG/Assets/Scripts/Enemies/DrackyBat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class DrackyBat : MonoBehaviour {
+
+    [SerializeField] Vector3 attackAlertRange = new Vector3(1f,1f,0);
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Vector3 moveRange = new Vector3(4f, 4f, 0);
+
+    PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Mathf.Abs( (player.transform.position - transform.position).magnitude ) <= attackAlertRange.magnitude)
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            MoveInPattern();
+        }
+	}
+
+    void FollowPlayer()
+    {
+        //TODO make follow player
+    }
+
+    void MoveInPattern()
+    {
+        //TODO move in pseudorandom pattern (prefferably in a box range)
+    }
+
+
+}
---
+++ Project - RPG/Assets/Scripts/Enemies/DrackyGizmos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrackyGizmos : MonoBehaviour {
+
+    DrackyBat dracky;
+
+    void Start()
+    {
+        dracky = GetComponentInChildren<DrackyBat>();
+    }
+
+    void OnDrawGizmos()
+    {
+        //MoveBox
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, dracky.moveRange);
+    }
+}
